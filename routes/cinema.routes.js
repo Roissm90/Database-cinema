f0eec01f77d6e6b383aeb0295946414ecf0a988b
rois90 <roissm90@gmail.com>
2023-11-26 13:57:01 +0100

    adding cinema.routes.js

--- routes/cinema.routes.js
+++ routes/cinema.routes.js
@@ -43,17 +43,12 @@ router.put('/add-movie', async (req, res, next) => {
     try {
         const cinemaId = req.body.cinemaId; 
         const movieId = req.body.movieId;
-
-        const updatedCinema = await Cinema.findByIdAndUpdate(
-            cinemaId,
-            { $push: { movies: movieId } },
-            { new: true }
-        );
-
+        const updatedCinema = await Cinema.findByIdAndUpdate(cinemaId, {
+            $push: { movies: movieId } 
+        });
         if (!updatedCinema) {
             return res.status(404).json({ error: 'Cinema no encontrado' });
         }
-
         return res.status(200).json(updatedCinema);
     } 
     catch (err) {
@@ -61,5 +56,56 @@ router.put('/add-movie', async (req, res, next) => {
     }
 });
 
+router.put('/add-movie-by-genre', async (req, res, next) => {
+    try {
+        const cinemaId = req.body.cinemaId; 
+        const genre = req.body.genre;
+        const moviesToAdd = await movieId.find({genre: genre});
+        const updatedCinema = await Cinema.findByIdAndUpdate(cinemaId, {
+            $addToSet: { movies: { $each: moviesToAdd}},
+            //new: true
+        })
+        return res.status(200).json(updatedCinema);
+    } 
+    catch (err) {
+        return next(err);
+    }
+});
+
+router.put('/add-movie-array', async (req, res, next) => {
+    try {
+        const cinemaId = req.body.cinemaId; 
+        const movieIdArray = req.body.movieIdArray;
+        const updatedCinema = await Cinema.findByIdAndUpdate(cinemaId, {
+            $addToSet: { movies: { $each: movieIdArray } }
+        });
+        if (!updatedCinema) {
+            return res.status(404).json({ error: 'Cinema no encontrado' });
+        }
+        return res.status(200).json(updatedCinema);
+    } 
+    catch (err) {
+        return next(err);
+    }
+});
+
+
+
+router.delete('/delete-movie-array/:id', async (req, res, next) => {
+    try {
+        const cinemaId = req.body.cinemaId; 
+        const movieIdArray = req.body.movieIdArray;
+        const updatedCinema = await Cinema.findByIdAndDelete(cinemaId, {
+            $pull: { movies: { $each: movieIdArray } }
+        });
+        if (!updatedCinema) {
+            return res.status(404).json({ error: 'Cinema no encontrado' });
+        }
+        return res.status(200).json(updatedCinema);
+    } 
+    catch (err) {
+        return next(err);
+    }
+});
 
 module.exports = router;
